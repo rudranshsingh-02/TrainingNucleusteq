aad14d33ea5f8c2bad3778028a21f1d66b2bca8d
Rudransh Singh Chouhan <singhchouhanrudransh@gmail.com>
2025-03-23 15:16:51 +0530

    Initial commit

---
+++ iwas-backend/src/main/java/com/iwas/iwas_backend/IwasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.iwas.iwas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwasBackendApplication.class, args);
+	}
+
+}
---
+++ iwas-backend/src/test/java/com/iwas/iwas_backend/IwasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iwas.iwas_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
