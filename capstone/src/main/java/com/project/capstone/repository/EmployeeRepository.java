f4d8f9a9b128c470c553899f13eb9f620af50710
rudranshsingh-02 <rudranshsinghchouhan02@gmail.com>
2025-04-10 01:52:35 +0530

    Update EmployeeRepository.java

--- capstone/src/main/java/com/project/capstone/repository/EmployeeRepository.java
+++ capstone/src/main/java/com/project/capstone/repository/EmployeeRepository.java
@@ -1,8 +1,15 @@
 package com.project.capstone.repository;
+
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.project.capstone.models.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
     Optional<Employee> findByEmail(String email);
+
+    @Query("SELECT e FROM Employee e LEFT JOIN FETCH e.assignedProjects WHERE e.id = :id")
+    Optional<Employee> findByIdWithProjects(@Param("id") Long id);
 }
