8b01c12e3f8cc34b153b9488fb3264113038e4bc
rudranshsingh-02 <rudranshsinghchouhan02@gmail.com>
2025-04-10 01:48:20 +0530

    Update LeaveRequestController.java

--- capstone/src/main/java/com/project/capstone/controllers/LeaveRequestController.java
+++ capstone/src/main/java/com/project/capstone/controllers/LeaveRequestController.java
@@ -1,13 +1,13 @@
 package com.project.capstone.controllers;
+
 import com.project.capstone.models.LeaveRequest;
 import com.project.capstone.models.LeaveStatus;
 import com.project.capstone.repository.LeaveRequestRepository;
 import com.project.capstone.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-// import java.util.Optional;
-
 
 @RestController
 @RequestMapping("/leaves")
@@ -20,29 +20,28 @@ public class LeaveRequestController {
     private EmployeeRepository employeeRepository;
 
     @PostMapping("/{employeeId}")
-    public LeaveRequest requestLeave(@PathVariable Long employeeId, @RequestBody LeaveRequest leaveRequest)
-    {
-        leaveRequest.setEmployee(employeeRepository.findById(employeeId).orElseThrow(()->new RuntimeException("Employee not found")));
+    public ResponseEntity<String> requestLeave(@PathVariable Long employeeId, @RequestBody LeaveRequest leaveRequest) {
+        leaveRequest.setEmployee(employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new RuntimeException("Employee not found")));
         leaveRequest.setStatus(LeaveStatus.PENDING);
-        return leaveRequestRepository.save(leaveRequest);
+        leaveRequestRepository.save(leaveRequest);
+        return ResponseEntity.ok("Leave request submitted successfully.");
     }
 
     @GetMapping
-    public List<LeaveRequest> getAllLeaveRequests()
-    {
+    public List<LeaveRequest> getAllLeaveRequests() {
         return leaveRequestRepository.findAll();
     }
 
     @GetMapping("/employee/{employeeId}")
-    public List<LeaveRequest> getEmployeeLeaves(@PathVariable Long employeeId)
-    {
+    public List<LeaveRequest> getEmployeeLeaves(@PathVariable Long employeeId) {
         return leaveRequestRepository.findByEmployeeId(employeeId);
     }
 
     @PutMapping("/{leaveId}/{status}")
-    public LeaveRequest updateLeaveStatus(@PathVariable Long leaveId,@PathVariable LeaveStatus status)
-    {
-        LeaveRequest leaveRequest=leaveRequestRepository.findById(leaveId).orElseThrow(()->new RuntimeException("leave not found"));
+    public LeaveRequest updateLeaveStatus(@PathVariable Long leaveId, @PathVariable LeaveStatus status) {
+        LeaveRequest leaveRequest = leaveRequestRepository.findById(leaveId)
+                .orElseThrow(() -> new RuntimeException("leave not found"));
         leaveRequest.setStatus(status);
         return leaveRequestRepository.save(leaveRequest);
     }
