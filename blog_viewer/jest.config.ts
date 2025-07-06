7bd5459eeeab35ea75a95c2b66192d86be5b56b1
rudranshsingh-02 <rudranshsinghchouhan02@gmail.com>
2025-07-06 17:09:47 +0530

    Add files via upload

---
+++ blog_viewer/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx)$': 'babel-jest', 
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+};
+
+export default config;
